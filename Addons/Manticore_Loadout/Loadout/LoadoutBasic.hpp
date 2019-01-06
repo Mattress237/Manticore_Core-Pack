93d22f70f0bb4495c21756ffbefd3ca4b81e3d3e
Mattress237 <38886009+Mattress237@users.noreply.github.com>
2019-01-06 12:59:07 +0100

    Added more macros for easier editing of loadout. Added the macros to LoadoutBasic and similar

--- Addons/Manticore_Loadout/Loadout/LoadoutBasic.hpp
+++ Addons/Manticore_Loadout/Loadout/LoadoutBasic.hpp
@@ -2,89 +2,27 @@
 
 class TransportWeapons
 {
-	_standardRifle(1);
-	/*class _xx_rhs_weap_hk416d145
-	{
-		weapon = "rhs_weap_hk416d145"; 
-		count = 1; 
-	};*/
+	_standardRifle(1, 0, 0);
+
 	_standardATTub(2);
-	/*class _xx_rhs_weap_M136
-	{
-		weapon = "rhs_weap_M136"; 
-		count = 2; 
-	};*/
+	
 
 };
 class TransportMagazines
 {
-	_standardMag(20,20);
-	/*class _xx_rhs_mag_30Rnd_556x45_Mk318_Stanag
-	{
-		magazine = "rhs_mag_30Rnd_556x45_Mk318_Stanag"; 
-		count = 20; 
-	}; 
-		class _xx_rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Red
-	{
-		magazine = "rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Red"; 
-		count = 20; 
-	};*/
+	_standardMag(20, 20);
 
-	class _xx_1Rnd_HE_Grenade_shell
-	{
-		magazine = "1Rnd_HE_Grenade_shell";
-		count = 10;
-	};
-	class _xx_UGL_FlareRed_F
-	{
-		magazine = "UGL_FlareRed_F";
-		count = 10;
-	};
-	class _xx_1Rnd_SmokeRed_Grenade_shell
-	{
-		magazine = "1Rnd_SmokeRed_Grenade_shell";
-		count = 10;
-	};
-	class _xx_rhs_mag_m67
-	{
-		magazine = "rhs_mag_m67";
-		count = 5;
-	};
-	class _xx_rhs_mag_mk84
-	{
-		magazine = "rhs_mag_mk84";
-		count = 5;
-	};
-	class _xx_rhs_ACE_HandFlare_Green
-	{
-		magazine = "ACE_HandFlare_Green";
-		count = 4;
-	};
-	class _xx_SmokeShellBlue
-	{
-		magazine = "SmokeShellBlue";
-		count = 2;
-	};
-	class _xx_SmokeShellPurple
-	{
-		magazine = "SmokeShellPurple";
-		count = 2;
-	};
-	class _xx_SmokeShellGreen
-	{
-		magazine = "SmokeShellGreen";
-		count = 2;
-	};
-	class _xx_SmokeShellRed
-	{
-		magazine = "SmokeShellRed";
-		count = 2;
-	};
-	class _xx_SmokeShell
-	{
-		magazine = "SmokeShell";
-		count = 10;
-	};
+	_UGLHE(10);
+
+	_UGLFlear(10, 0, 0, 0, 0);
+
+	_UGLSmoke(10, 0, 0, 0, 0, 0, 0);
+	
+	_GrenadeOffensive(5, 5, 2);
+
+	_GrenadeFlear(0, 0, 4, 0);
+	
+	_GrenadeSmoke(2, 10, 2, 0, 0, 1, 3);
 };
 class TransportItems
 {
@@ -93,93 +31,22 @@ class TransportItems
 		name = "Toolkit";
 		count = 1;
 	};
-	_advMedical(1,6)
-	/*class _xx_adv_aceCPR_AED
-	{
-		name = "adv_aceCPR_AED";
-		count = 1;
-	};
-	class _xx_adv_aceSplint_splint
-	{
-		name = "adv_aceSplint_splint";
-		count = 6;
-	};*/
-	_basicMedical(10,10,10,10);
-	/*class _xx_ACE_fieldDressing
-	{
-		name = "ACE_fieldDressing"; 
-		count = 10; 
-	}; 
-	class _xx_ACE_elasticBandage
-	{
-		name = "ACE_elasticBandage"; 
-		count = 10; 
-	}; 
-	class _xx_ACE_quikclot
-	{
-		name = "ACE_quikclot"; 
-		count = 10; 
-	}; 
-	class _xx_ACE_packingBandage
-	{
-		name = "ACE_packingBandage"; 
-		count = 10; 
-	};*/
-	_injectorMedical(8,8,0,0);
-	/*class _xx_ACE_morphine
-	{
-		name = "ACE_morphine";
-		count = 8;
-	};
-	class _xx_ACE_epinephrine
-	{
-		name = "ACE_epinephrine";
-		count = 8;
-	};*/
-	_selinMedical(0,2,1);
-	/*class _xx_ACE_salineIV
-	{
-		name = "ACE_salineIV";
-		count = 2;
-	};*/
-	_micsMedical(8,4,0,0);
-	/*class _xx_ACE_tourniquet
-	{
-		name = "ACE_tourniquet";
-		count = 8;
-	};
-	class _xx_ACE_bodyBag
-	{
-		name = "ACE_bodyBag";
-		count = 4;
-	};*/
-	_aceMics(5,2,1);
-	/*class _xx_ACE_CableTie
-	{
-		name = "ACE_CableTie";
-		count = 5;
-	};
-	class _xx_ACE_EntrenchingTool
-	{
-		name = "ACE_EntrenchingTool";
-		count = 2;
-	};
-	class _xx_ACE_microDAGR
-	{
-		name = "ACE_microDAGR";
-		count = 1;
-	};*/
-	_aceXFood(4,0,4,0,0,0,0,0,0,4,0,0,0,0,0);
-	/*class _xx_ACE_Canteen
-	{
-		name = "ACE_Canteen";
-		count = 4;
-	};
-	class _xx_ACE_MRE_BeefStew
-	{
-		name = "ACE_MRE_BeefStew";
-		count = 4;
-	};*/
+
+	_advMedical(1, 6);
+
+	_basicMedical(10, 10, 10, 10);
+
+	_injectorMedical(8, 8, 0, 0);
+
+	_selinMedical(0, 2, 1);
+
+	_micsMedical(8, 4, 0, 0);
+
+	_aceMics(5, 2, 1);
+
+	_aceDrink(4, 0, 0, 0, 0);
+
+	_aceXFood(4, 0, 0, 0, 0, 0, 0, 4, 0, 0);
 	
 	class _xx_rhsusf_ANPVS_15
 	{
--- Addons/Manticore_Loadout/LoadoutMacro.hpp
+++ Addons/Manticore_Loadout/LoadoutMacro.hpp
@@ -1,21 +1,109 @@
-#define _standardRifle(Rifle)\
-	class _xx_rhs_weap_hk416d145\
+#define _standardRifle(StandardRifle,UGLRifle,CompactRifle)\
+	class _xx_hlc_rifle_G36KA1KSK\
 	{\
-		weapon = "rhs_weap_hk416d145";\
-		count = Rifle ;\
+		weapon = "hlc_rifle_G36KA1KSK";\
+		count = StandardRifle ;\
+	};\
+	class _xx_HLC_Rifle_G36KSKAG36\
+	{\
+		weapon = "HLC_Rifle_G36KSKAG36";\
+		count = UGLRifle ;\
+	};\
+	class _xx_hlc_rifle_G36C\
+	{\
+		weapon = "hlc_rifle_G36C";\
+		count = CompactRifle ;\
 	};
 
 #define _standardMag(Normal,Tracer)\
-	class _xx_rhs_mag_30Rnd_556x45_Mk318_Stanag\
+	class _xx_hlc_30rnd_556x45_EPR_G36\
 	{\
-		magazine = "rhs_mag_30Rnd_556x45_Mk318_Stanag";\
+		magazine = "hlc_30rnd_556x45_EPR_G36";\
 		count = Normal;\
 	};\
-	class _xx_rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Red\
+	class _xx_hlc_30rnd_556x45_Tracers_G36\
 	{\
-		magazine = "rhs_mag_30Rnd_556x45_M855_Stanag_Tracer_Red";\
+		magazine = "hlc_30rnd_556x45_Tracers_G36";\
 		count = Tracer;\
 	}; 
+
+#define _UGLSmoke(Red,White,Green,Yellow,Orange,Purple,Blue)\
+	class _xx_1Rnd_SmokeRed_Grenade_shell\
+	{\
+		magazine = "1Rnd_SmokeRed_Grenade_shell";\
+		count = Red;\
+	};\
+	class _xx_1Rnd_Smoke_Grenade_shell\
+	{\
+		magazine = "1Rnd_Smoke_Grenade_shell";\
+		count = White;\
+	};\
+	class _xx_1Rnd_SmokeGreen_Grenade_shell\
+	{\
+		magazine = "1Rnd_SmokeGreen_Grenade_shell";\
+		count = Green;\
+	};\
+	class _xx_1Rnd_SmokeYellow_Grenade_shell\
+	{\
+		magazine = "1Rnd_SmokeYellow_Grenade_shell";\
+		count = Yellow;\
+	};\
+	class _xx_1Rnd_SmokeOrange_Grenade_shell\
+	{\
+		magazine = "1Rnd_SmokeOrange_Grenade_shell";\
+		count = Orange;\
+	};\
+	class _xx_1Rnd_SmokePurple_Grenade_shell\
+	{\
+		magazine = "1Rnd_SmokePurple_Grenade_shell";\
+		count = Purple;\
+	};\
+	class _xx_1Rnd_SmokeBlue_Grenade_shell\
+	{\
+		magazine = "1Rnd_SmokeBlue_Grenade_shell";\
+		count = Blue;\
+	};
+	
+#define _UGLFlear(Red,White,Green,Yellow,IRFlare)\
+	class _xx_UGL_FlareRed_F\
+	{\
+		magazine = "UGL_FlareRed_F";\
+		count = Red;\
+	};\
+	class _xx_UGL_FlareWhite_F\
+	{\
+		magazine = "UGL_FlareWhite_F";\
+		count = White;\
+	};\
+	class _xx_UGL_FlareGreen_F\
+	{\
+		magazine = "UGL_FlareGreen_F";\
+		count = Green;\
+	};\
+	class _xx_UGL_FlareYellow_F\
+	{\
+		magazine = "UGL_FlareYellow_F";\
+		count = Yellow;\
+	};\
+	class _xx_UGL_FlareCIR_F\
+	{\
+		magazine = "UGL_FlareCIR_F";\
+		count = IRFlare;\
+	};
+
+#define _UGLHE(Grenade)\
+	class _xx_1Rnd_HE_Grenade_shell\
+	{\
+		magazine = "1Rnd_HE_Grenade_shell";\
+		count = Grenade;\
+	};
+
+#define _UGLMics(HuntIR)\
+	class _xx_ACE_HuntIR_M203\
+	{\
+		magazine = "ACE_HuntIR_M203";\
+		count = HuntIR;\
+	};
 
 #define _standardATTub(ATtub)\
 	class _xx_rhs_weap_M136\
@@ -23,6 +111,91 @@
 		weapon = "rhs_weap_M136";\
 		count = ATtub ;\
 	}; 
+//
+#define _GrenadeOffensive(Frag,Stun,Incendiary)\
+	class _xx_rhs_mag_m67\
+	{\
+		weapon = "rhs_mag_m67";\
+		count = Frag;\
+	};\
+	class _xx_rhs_mag_mk84\
+	{\
+		weapon = "rhs_mag_mk84";\
+		count = Stun;\
+	};\
+	class _xx_rhs_mag_an_m14_th3\
+	{\
+		weapon = "rhs_mag_an_m14_th3";\
+		count = Incendiary;\
+	};
+
+#define _GrenadeSmoke(Red,White,Green,Yellow,Orange,Purple,Blue)\
+	class _xx_SmokeShellRed\
+	{\
+		weapon = "SmokeShellRed";\
+		count = Red;\
+	};\
+	class _xx_SmokeShell\
+	{\
+		weapon = "SmokeShell";\
+		count = White;\
+	};\
+	class _xx_SmokeShellGreen\
+	{\
+		weapon = "SmokeShellGreen";\
+		count = Green;\
+	};\
+	class _xx_SmokeShellYellow\
+	{\
+		weapon = "SmokeShellYellow";\
+		count = Yellow;\
+	};\
+	class _xx_SmokeShellOrange\
+	{\
+		weapon = "SmokeShellOrange";\
+		count = Orange;\
+	};\
+	class _xx_SmokeShellPurple\
+	{\
+		weapon = "SmokeShellPurple";\
+		count = Purple;\
+	};\
+	class _xx_SmokeShellBlue\
+	{\
+		weapon = "SmokeShellBlue";\
+		count = Blue;\
+	};
+
+#define _GrenadeFlear(Red,White,Green,Yellow)\
+	class _xx_ACE_HandFlare_Red\
+	{\
+		weapon = "ACE_HandFlare_Red";\
+		count = Red;\
+	};\
+	class _xx_ACE_HandFlare_White\
+	{\
+		weapon = "ACE_HandFlare_White";\
+		count = White;\
+	};\
+	class _xx_ACE_HandFlare_Green\
+	{\
+		weapon = "ACE_HandFlare_Green";\
+		count = Green;\
+	};\
+	class _xx_ACE_HandFlare_Yellow\
+	{\
+		weapon = "ACE_HandFlare_Yellow";\
+		count = Yellow;\
+	};
+
+#define _GrenadeIR(IRGrenade)\
+	class _xx_B_IR_Grenade\
+	{\
+		weapon = "B_IR_Grenade";\
+		count =  IRGrenade;\
+	};
+
+
 
 //Medical
 #define _basicMedical(Field,Elastic,Quikclot,Packing)\
@@ -120,7 +293,7 @@
 		count = SurgicalKit;\
 	};
 
-#define _aceXFood(Canteen,Water,MRE0,MRE1,MRE2,MRE3,MRE4,MRE5,MRE6,MRE7,Banana,MREHR,Spirit,RedGull,Franta)\
+#define _aceDrink(Canteen,Water,Spirit,RedGull,Franta)\
 	class _xx_ACE_Canteen\
 	{\
 		name = "ACE_Canteen";\
@@ -131,6 +304,23 @@
 		name = "ACE_WaterBottle";\
 		count = Water;\
 	};\
+	class _xx_ACE_Can_Spirit\
+	{\
+		name = "ACE_Can_Spirit";\
+		count = Spirit;\
+	};\
+	class _xx_ACE_Can_RedGull\
+	{\
+		name = "ACE_Can_RedGull";\
+		count = RedGull;\
+	};\
+	class _xx_ACE_Can_Franta\
+	{\
+		name = "ACE_Can_Franta";\
+		count = Franta;\
+	};
+
+#define _aceXFood(MRE0,MRE1,MRE2,MRE3,MRE4,MRE5,MRE6,MRE7,Banana,MREHR)\
 	class _xx_ACE_MRE_BeefStew\
 	{\
 		name = "ACE_MRE_BeefStew";\
@@ -180,22 +370,8 @@
 	{\
 		name = "ACE_Humanitarian_Ration";\
 		count = MREHR;\
-	};\
-	class _xx_ACE_Can_Spirit\
-	{\
-		name = "ACE_Can_Spirit";\
-		count = Spirit;\
-	};\
-	class _xx_ACE_Can_RedGull\
-	{\
-		name = "ACE_Can_RedGull";\
-		count = RedGull;\
-	};\
-	class _xx_ACE_Can_Franta\
-	{\
-		name = "ACE_Can_Franta";\
-		count = Franta;\
-	};\
+	};
+	
 
 #define _aceMics(CableTie,EntrenchingTool,DAGR )\
 	class _xx_ACE_CableTie\
