97c6837264b53efadfc4c5e28d7a300cf389818e
Mattress237 <38886009+Mattress237@users.noreply.github.com>
2019-01-26 00:32:58 +0100

    Added - Paraclete RAV (Gunner) Vest Removed - Manticam Winter Crye Test classes and files

--- Addons/Manticore_Uniform/Uniform/Winter/config.cpp
+++ Addons/Manticore_Uniform/Uniform/Winter/config.cpp
@@ -21,13 +21,13 @@ class CfgPatches
 class CfgVehicles
 {
 	class VSM_Multicam_Crye_Uniform;
-	class Manticore_Uniform_Uniform_Winter_Crye_Test_00 : VSM_Multicam_Crye_Uniform
+	class Manticore_Uniform_Uniform_Winter_Crye : VSM_Multicam_Crye_Uniform
 	{
 		author = "Mattress, VanSchmoozin";
 		scope = 2;
-		displayName = "Manticore Winter Camo TEST (00)";
+		displayName = "Manticore Winter Camo (WIP)";
 		faction = "Manticore_Faction";
-		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_00";
+		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye";
 		hiddenSelections[] =
 		{
 			"Camo",
@@ -36,236 +36,6 @@ class CfgVehicles
 		hiddenSelectionsTextures[] =
 		{
 			"\Manticore_Uniform\Uniform\Winter\Data\VSM_ARD_Test_00.paa"
-		};
-		weapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		respawnWeapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		magazines[] =
-		{
-			
-		};
-		respawnMagazines[] =
-		{
-			
-		};
-		linkedItems[] =
-		{
-			
-		};
-		respawnLinkedItems[] =
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_Test_01 : VSM_Multicam_Crye_Uniform
-	{
-		author = "Mattress, VanSchmoozin";
-		scope = 2;
-		displayName = "Manticore Winter Camo TEST (01)";
-		faction = "Manticore_Faction";
-		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_01";
-		hiddenSelections[] =
-		{
-			"Camo",
-			"Insignia"
-		};
-		hiddenSelectionsTextures[] =
-		{
-			"\Manticore_Uniform\Uniform\Winter\Data\VSM_ARD_Test_01.paa"
-		};
-		weapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		respawnWeapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		magazines[] =
-		{
-
-		};
-		respawnMagazines[] =
-		{
-
-		};
-		linkedItems[] =
-		{
-
-		};
-		respawnLinkedItems[] =
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_Test_02 : VSM_Multicam_Crye_Uniform
-	{
-		author = "Mattress, VanSchmoozin";
-		scope = 2;
-		displayName = "Manticore Winter Camo TEST (02)";
-		faction = "Manticore_Faction";
-		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_02";
-		hiddenSelections[] =
-		{
-			"Camo",
-			"Insignia"
-		};
-		hiddenSelectionsTextures[] =
-		{
-			"\Manticore_Uniform\Uniform\Winter\Data\VSM_ARD_Test_02.paa"
-		};
-		weapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		respawnWeapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		magazines[] =
-		{
-
-		};
-		respawnMagazines[] =
-		{
-
-		};
-		linkedItems[] =
-		{
-
-		};
-		respawnLinkedItems[] =
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_Test_03 : VSM_Multicam_Crye_Uniform
-	{
-		author = "Mattress, VanSchmoozin";
-		scope = 2;
-		displayName = "Manticore Winter Camo TEST (03)";
-		faction = "Manticore_Faction";
-		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_03";
-		hiddenSelections[] =
-		{
-			"Camo",
-			"Insignia"
-		};
-		hiddenSelectionsTextures[] =
-		{
-			"\Manticore_Uniform\Uniform\Winter\Data\VSM_ARD_Test_03.paa"
-		};
-		weapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		respawnWeapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		magazines[] =
-		{
-
-		};
-		respawnMagazines[] =
-		{
-
-		};
-		linkedItems[] =
-		{
-
-		};
-		respawnLinkedItems[] =
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_Test_04 : VSM_Multicam_Crye_Uniform
-	{
-		author = "Mattress, VanSchmoozin";
-		scope = 2;
-		displayName = "Manticore Winter Camo TEST (04)";
-		faction = "Manticore_Faction";
-		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_04";
-		hiddenSelections[] =
-		{
-			"Camo",
-			"Insignia"
-		};
-		hiddenSelectionsTextures[] =
-		{
-			"\Manticore_Uniform\Uniform\Winter\Data\VSM_ARD_Test_04.paa"
-		};
-		weapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		respawnWeapons[] =
-		{
-			"Throw",
-			"Put"
-		};
-		magazines[] =
-		{
-
-		};
-		respawnMagazines[] =
-		{
-
-		};
-		linkedItems[] =
-		{
-
-		};
-		respawnLinkedItems[] =
-		{
-			"ItemMap",
-			"ItemCompass",
-			"ItemWatch",
-			"ItemRadio"
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_Test_05 : VSM_Multicam_Crye_Uniform
-	{
-		author = "Mattress, VanSchmoozin";
-		scope = 2;
-		displayName = "Manticore Winter Camo TEST (05)";
-		faction = "Manticore_Faction";
-		uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_05";
-		hiddenSelections[] =
-		{
-			"Camo",
-			"Insignia"
-		};
-		hiddenSelectionsTextures[] =
-		{
-			"\Manticore_Uniform\Uniform\Winter\Data\VSM_ARD_Test_05.paa"
 		};
 		weapons[] =
 		{
@@ -382,91 +152,26 @@ class cfgWeapons
 		class ItemInfo;
 	};
 	class VSM_Multicam_Crye_Camo;
-	class Manticore_Uniform_Uniform_Winter_Crye_00 : VSM_Multicam_Crye_Camo
+	class Manticore_Uniform_Uniform_Winter_Crye : VSM_Multicam_Crye_Camo
 	{
 		scope = 2;
-		displayName = "Manticore Winter Camo Test (00)";
+		displayName = "Manticore Winter Crye";
 		author = "Mattress, VanSchmoozin";
 		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
 		class ItemInfo : UniformItem
 		{
-			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_Test_00";
-			containerClass = "Supply70";
-			mass = 35;
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_01 : VSM_Multicam_Crye_Camo
-	{
-		scope = 2;
-		displayName = "Manticore Winter Camo Test (01)";
-		author = "Mattress, VanSchmoozin";
-		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
-		class ItemInfo : UniformItem
-		{
-			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_Test_01";
-			containerClass = "Supply70";
-			mass = 35;
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_02 : VSM_Multicam_Crye_Camo
-	{
-		scope = 2;
-		displayName = "Manticore Winter Camo Test (02)";
-		author = "Mattress, VanSchmoozin";
-		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
-		class ItemInfo : UniformItem
-		{
-			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_Test_02";
-			containerClass = "Supply70";
-			mass = 35;
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_03 : VSM_Multicam_Crye_Camo
-	{
-		scope = 2;
-		displayName = "Manticore Winter Camo Test (03)";
-		author = "Mattress, VanSchmoozin";
-		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
-		class ItemInfo : UniformItem
-		{
-			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_Test_03";
-			containerClass = "Supply70";
-			mass = 35;
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_04 : VSM_Multicam_Crye_Camo
-	{
-		scope = 2;
-		displayName = "Manticore Winter Camo Test (04)";
-		author = "Mattress, VanSchmoozin";
-		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
-		class ItemInfo : UniformItem
-		{
-			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_Test_04";
-			containerClass = "Supply70";
-			mass = 35;
-		};
-	};
-	class Manticore_Uniform_Uniform_Winter_Crye_05 : VSM_Multicam_Crye_Camo
-	{
-		scope = 2;
-		displayName = "Manticore Winter Camo Test (05)";
-		author = "Mattress, VanSchmoozin";
-		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
-		class ItemInfo : UniformItem
-		{
-			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye_Test_05";
+			uniformClass = "Manticore_Uniform_Uniform_Winter_Crye";
 			containerClass = "Supply70";
 			mass = 35;
 		};
 	};
 
 	class VSM_RAV_operator_MulticamTropic;
-	class Manticore_Uniform_Uniform_Winter_RAV_operator_00 : VSM_RAV_operator_MulticamTropic
+	class Manticore_Uniform_Uniform_Winter_RAV_Operator : VSM_RAV_operator_MulticamTropic
 	{
 		scope = 2;
 		author = "Mattress, VanSchmoozin - Specter";
-		displayName = "Manticam Winter Paraclete RAV(Operator)(WIP)";
+		displayName = "Manticam Winter Paraclete RAV(Operator)";
 		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
 		hiddenSelections[] =
 		{
@@ -483,6 +188,40 @@ class cfgWeapons
 		{
 			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Chestrig_00.paa",
 			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_BattleBelt_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_DropHolster_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Serpa_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Gear_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Radio_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_RAV_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Pouches_00.paa"
+		};
+		//descriptionShort = "$STR_A3_SP_AL_IV";
+	};
+
+	class VSM_RAV_MG_MulticamTropic;
+	class Manticore_Uniform_Uniform_Winter_RAV_Gunner : VSM_RAV_MG_MulticamTropic
+	{
+		scope = 2;
+		author = "Mattress, VanSchmoozin - Specter";
+		displayName = "Manticam Winter Paraclete RAV (Gunner)";
+		picture = "\Manticore_Uniform\Uniform\Winter\Data\Icon\Manticam_Logo_Winter.paa";
+		hiddenSelections[] =
+		{
+			"_1961aPouches",
+			"_battleBelt",
+			"_boxmag",
+			"_DropHolster",
+			"_Serpa",
+			"_SOTGGear",
+			"_SOTGRadio",
+			"_vestBase",
+			"_VSMPouches"
+		};
+		hiddenSelectionsTextures[] =
+		{
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Chestrig_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_BattleBelt_00.paa",
+			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_BoxMag_00.paa",
 			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_DropHolster_00.paa",
 			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Serpa_00.paa",
 			"\Manticore_Uniform\Uniform\Winter\Data\VSM_Vest_Gear_00.paa",
